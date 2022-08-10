aaa608c39738b7172f4db038e62f497419907983
luiizsilverio <luiiz.silverio@gmail.com>
2022-08-09 21:16:13 -0300

    Routes + Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="app">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import user from "../../images/user.png";
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="header">
+      <Link to="/">
+        <div className="logo">My-Movies</div>
+      </Link>
+      <div className="user-image">
+        <img src={user} alt="user" />
+      </div>
     </div>
   );
 };
