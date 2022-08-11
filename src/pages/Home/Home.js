e01344bf32207dc911c83b9e72b2458018a7269b
luiizsilverio <luiiz.silverio@gmail.com>
2022-08-11 20:56:49 -0300

    Fetch movies from api

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useEffect } from 'react';
+import MovieList from '../../components/MovieList/MovieList';
+import movieApi from '../../common/apis/movieApi';
 
 const Home = () => {
+
+  useEffect(() => {
+    const movieText = "Duna";
+
+    const fetchMovies = async () => {
+      const response = await movieApi.get(
+        `?apiKey=${process.env.REACT_APP_OMDB_API_KEY}&s=${movieText}&type=movie`
+      )
+      .catch((err) => {
+        console.warn("Erro:", err)
+      })
+      console.log(response.data);
+    }
+
+    fetchMovies();
+  }, [])
+
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <div className='banner-img'></div>
+      <MovieList />
+    </>
   );
 };
 
