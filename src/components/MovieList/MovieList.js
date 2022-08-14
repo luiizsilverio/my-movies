90d3b0d6436d784ae5f9c0ef3b6807d798e35fe3
luiizsilverio <luiiz.silverio@gmail.com>
2022-08-14 17:34:01 -0300

    Carrossel com react-slick

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Slider from "react-slick";
+
 import { getAllMovies, getAllSeries } from '../../redux/movies/movieSlice';
 import MovieCard from '../MovieCard/MovieCard';
+import { SliderSettings } from '../../common/sliderSettings';
 import "./MovieList.scss";
 
 const MovieList = () => {
@@ -11,15 +14,25 @@ const MovieList = () => {
   const renderMovies = movies ? movies.Response === "True" : null;
   const renderShows = series ? series.Response === "True" : null;
 
+  function renderCarrossel(lista) {
+    return (
+      <Slider {...SliderSettings}>
+        {
+          lista.Search.map((movie) => (
+            <MovieCard key={movie.imdbID} data={movie} />
+          ))
+        }
+      </Slider>
+    )
+  }
+
   return (
     <div className='movie-wrapper'>
       <div className="movie-list">
         <h2>Filmes</h2>
         <div className="movie-container">
           {renderMovies ? (
-            movies.Search.map((movie) => (
-              <MovieCard key={movie.imdbID} data={movie} />
-            ))
+            renderCarrossel(movies)
           ) : (
             <div className="movies-error">
               <h3>{movies && movies.Error}</h3>
@@ -32,9 +45,7 @@ const MovieList = () => {
         <h2>Séries</h2>
         <div className="movie-container">
           {renderShows ? (
-            series.Search.map((serie) => (
-              <MovieCard key={serie.imdbID} data={serie} />
-            ))
+            renderCarrossel(series)
           ) : (
             <div className="movies-error">
               <h3>{series && series.Error}</h3>
