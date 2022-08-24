77aa597b7b57db1c349d8f4f320eb83e47380c30
luiizsilverio <luiiz.silverio@gmail.com>
2022-08-24 19:36:27 -0300

    remover </React.StrictMode> do index.js

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-      <ToastContainer autoClose={3000} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+    <ToastContainer autoClose={3000} />
+  </Provider>
 );
